efa4cdc0bf6b941ab5cf2268380b6f96edf70190
wonseok <rhdqngkrh77@khu.ac.kr>
2023-08-08 22:50:03 +0900

    practice

--- src/pages/NavigateEnd.js
+++ src/pages/NavigateEnd.js
@@ -1,16 +1,15 @@
 import "../components/page.css";
 
 function NavigateEnd() {
-    return(
-        <div className="basePage">
-            <h2>
-                목적지에<br/>
-                도착했습니다<br/>
-                안내를 종료합니다
-            </h2>
-
-        </div>
-
-
-    );
+  return (
+    <div className="basePage">
+      <h2>
+        목적지에
+        <br />
+        도착했 습니다
+        <br />
+        안내를 종료합니다
+      </h2>
+    </div>
+  );
 }
